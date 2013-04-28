9f4e9e2bb807ced4fcef3c989d7337750e716489
jasallen <github@deboever.us>
2013-04-28 19:04:02 -0400

    added call to HN 'the wrong way' - locks browser

--- mobiDevDayPrep/js/controllers.js
+++ mobiDevDayPrep/js/controllers.js
@@ -4,12 +4,33 @@
 function homeCtrl ($scope, $http) {
 
 	$scope.subRedditName = "technology";
-
+	
 	$scope.fetch = function(){
 		$http.jsonp("http://www.reddit.com/r/" + $scope.subRedditName + ".json?jsonp=JSON_CALLBACK")
 		.success(function(data, status) {
 			$scope.reddit = data;
 		});
 	};
+
+	$scope.getKeys = function(obj){
+		return Object.keys(obj);
+	};
+
+	$scope.HnHasMatch = function(url){
+		var ret = false;
+
+		$scope.fetchHn(url)
+		.success(function(data, status) {
+			ret = data.hits > 0;
+		});
+
+		return ret;
+	}
+
+	$scope.fetchHn = function(url){
+		return $http.jsonp("http://api.thriftdb.com/api.hnsearch.com/items/_search?q=" + url + "?callback=JSON_CALLBACK")		
+	};
+
+	$scope.fetch();
 
 };
