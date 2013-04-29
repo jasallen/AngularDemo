6f22032eb9ce4178f90a8bc428b701c71b584ad4
jasallen <github@deboever.us>
2013-04-29 06:37:35 -0400

    fix call to HN. loop the $http call.

--- mobiDevDayPrep/js/controllers.js
+++ mobiDevDayPrep/js/controllers.js
@@ -10,12 +10,12 @@ function homeCtrl ($scope, $http) {
 		.success(function(data, status) {
 			$scope.reddit = data;
 
+			for (var i = data.data.children.length - 1; i >= 0; i--) {
+			 	data.data.children[i].foundOnHn = $http.jsonp("http://api.thriftdb.com/api.hnsearch.com/items/_search?q=" + data.data.children[i].data.url + "&callback=JSON_CALLBACK") ;
+			 	//HackerNews.get(data.data.children[i].data.url);		 	
+			};
 		});
 	};
-
-	$scope.foundOnHn = function(url) {
-		$http.jsonp("http://api.thriftdb.com/api.hnsearch.com/items/_search?q=" + url + "&callback=JSON_CALLBACK") ;
-	}
 
 	$scope.getKeys = function(obj){
 		return Object.keys(obj);
