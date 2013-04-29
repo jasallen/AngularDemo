b8194e7e58cb7284d365a8c323850435a5a5b27f
jasallen <github@deboever.us>
2013-04-29 06:17:33 -0400

    added call to HN 'the wrong way' - locks browser

--- mobiDevDayPrep/js/controllers.js
+++ mobiDevDayPrep/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict'
 
 
-function homeCtrl ($scope, $http) {
+function homeCtrl ($scope, $http, $q, HackerNews) {
 
 	$scope.subRedditName = "technology";
 	
@@ -9,28 +9,20 @@ function homeCtrl ($scope, $http) {
 		$http.jsonp("http://www.reddit.com/r/" + $scope.subRedditName + ".json?jsonp=JSON_CALLBACK")
 		.success(function(data, status) {
 			$scope.reddit = data;
+
+			// for (var i = data.data.children.length - 1; i >= 0; i--) {
+			//  	data.data.children[i].foundOnHn = $http.jsonp("http://api.thriftdb.com/api.hnsearch.com/items/_search?q=" + data.data.children[i].data.url + "&callback=JSON_CALLBACK") ;
+			//  	//HackerNews.get(data.data.children[i].data.url);		 	
+			// };
 		});
 	};
+
+	$scope.foundOnHn = function(url) {
+		$http.jsonp("http://api.thriftdb.com/api.hnsearch.com/items/_search?q=" + url + "&callback=JSON_CALLBACK") ;
+	}
 
 	$scope.getKeys = function(obj){
 		return Object.keys(obj);
 	};
-
-	$scope.HnHasMatch = function(url){
-		var ret = false;
-
-		$scope.fetchHn(url)
-		.success(function(data, status) {
-			ret = data.hits > 0;
-		});
-
-		return ret;
-	}
-
-	$scope.fetchHn = function(url){
-		return $http.jsonp("http://api.thriftdb.com/api.hnsearch.com/items/_search?q=" + url + "?callback=JSON_CALLBACK")		
-	};
-
-	$scope.fetch();
 
 };
